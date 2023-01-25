77471cbca6f7eaa0ca8f804dc23e4a107396dbcb
Prohor <prohor-yakovlev@mail.ru>
2023-01-25 15:25:43 +0300

    styled-component

--- src/City.tsx
+++ src/City.tsx
@@ -1,30 +1,45 @@
 import React from 'react';
 import {CurrentBankomat} from "./CurrentBankomat";
 import {MoneyType} from "./App";
+import styled from "styled-components";
 
 type CityPropsType = {
-    data: any //встречаем денюжки
+    data: MoneyType[] //встречаем денюжки
 }
 
 export const City = (props: CityPropsType) => {
 
-    // пока это пропускам
-    // const mappedMoney = props.data.map((el: MoneyType, index) => (
-    //     <CurrentBankomat
-    //         key={index}
-    //         money={el}
-    //     />
-    // ))
-
+    const mappedMoney = props.data.map((el: MoneyType, index) => (
+        <CurrentBankomat
+            key={index}
+            money={el}
+        />
+    ))
+    // const mappedMoney = props.data.map(el => {
+    //     return (
+    //         <div>
+    //             <span>{el.value}</span>
+    //             <span>{el.number}</span>
+    //             <span>{el.banknotes}</span>
+    //         </div>
+    //     )
+    // })
 
     return (
-        <div>
-            <div>Ну все парни, мапимся -выводим наши денюжки</div>
-            <div>На верстку ПОКА не обращаем внимания , сейчас занимаемся логикой</div>
-            {/*<div>{mappedMoney}</div>*/}
-        </div>
+        <Wrapper>
+            {mappedMoney}
+        </Wrapper>
     );
 };
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  font-size: 30px;
+ 
+`
+
 //1
 // Вроде все норм, но нужно причесать код. Давайте создадим const mappedMoney = props.data.map(el=>el...)
 // Т.е. нам нужно вынести map из вертски, оставив в верстке только mappedMoney
--- src/CurrentBankomat.tsx
+++ src/CurrentBankomat.tsx
@@ -1,14 +1,67 @@
 import React from 'react';
 import {MoneyType} from "./App";
+import styled from "styled-components";
 
 type CurrentBankomatPropsType = {
     money: MoneyType
 }
-export const CurrentBankomat = (props:CurrentBankomatPropsType) => {
+export const CurrentBankomat = (props: CurrentBankomatPropsType) => {
 
     return (
-        <div>
-            {props.money.banknotes}
-        </div>
+
+        // props.money.banknotes === 'Dollars'
+        //     ? <BanknoteGreen>
+        //         <Name>{props.money.banknotes}</Name>
+        //         <Nominal>{props.money.value}</Nominal>
+        //     </BanknoteGreen>
+        //     : <BanknoteBlue>
+        //         <Name>{props.money.banknotes}</Name>
+        //         <Nominal>{props.money.value}</Nominal>
+        //     </BanknoteBlue>
+
+        <BanknoteUniversal color={props.money.banknotes === 'Dollars'? 'greenyellow': 'steelblue'}>
+            <Name>{props.money.banknotes}</Name>
+            <Nominal>{props.money.value}</Nominal>
+        </BanknoteUniversal>
     );
 };
+
+const BanknoteUniversal = styled.div`
+  background-color: ${props => {
+    if (props.color === 'greenyellow') {
+      return 'greenyellow'
+    } else {
+      return 'steelblue'
+    }
+  }};
+  width: 400px;
+  height: 200px;
+  margin: 10px;
+
+`
+
+const BanknoteGreen = styled.div`
+  background-color: greenyellow;
+  width: 400px;
+  height: 200px;
+  margin: 10px;
+
+`
+const BanknoteBlue = styled.div`
+  background-color: steelblue;
+  width: 400px;
+  height: 200px;
+  margin: 10px;
+`
+
+const Name = styled.div`
+  display: flex;
+  justify-content: center;
+  font-size: 30px;
+`
+const Nominal = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 50px;
+  font-size: 20px;
+`
